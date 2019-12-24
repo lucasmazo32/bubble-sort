3615f55f49bb189e62cbf051dc5bcdce9e85603b
Yosept <yosept.flores@gmail.com>
2019-12-24 16:28:58 -0600

    added to_s and a map, so that the result looks prettier

--- bubble.rb
+++ bubble.rb
@@ -1,20 +1,23 @@
-puts "Type the matrix you want to bubble sort (the matrix will be written without brackets and spaces between numbers like this e.g. 1 4 3 87 2): "
-arr_1 = gets.chomp
-arr_1 = arr_1.split("\s")
+# frozen_string_literal: true
+
+puts 'Type the matrix you want to bubble sort (the matrix will be written'
+puts 'without brackets and spaces between numbers like this e.g. 1 4 3 87 2): '
+
+arr1 = gets.chomp
+arr1 = arr1.split(' ').map(&:to_i)
 
 def bubble_sort(arr)
-
-    for i in 0...arr.length()-1
-        for j in 0...arr.length() -1 -i
-            a = arr[j]
-            b= arr[j+1]
-            if a > b
-                arr[j] = b
-                arr[j+1] = a
-            end
-        end
+  (0...arr.length - 1).each do |i|
+    (0...arr.length - 1 - i).each do |j|
+      a = arr[j]
+      b = arr[j + 1]
+      if a > b
+        arr[j] = b
+        arr[j + 1] = a
+      end
     end
-    return arr
+  end
+  arr.to_s
 end
 
-puts bubble_sort(arr_1)
+puts bubble_sort(arr1)
